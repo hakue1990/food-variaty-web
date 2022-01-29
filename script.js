e1fecf81981a53a2e3148beedd470a283b43f602
Adam <adam.haldas@gmail.com>
2022-01-29 17:28:59 +0100

    add mobile menu animations

--- script.js
+++ script.js
@@ -3,27 +3,39 @@
 const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1;
 
-setInterval(()=>{
-i++;
+setInterval(() => {
+  i++;
 
-const icon = document.querySelector('.section-1-icons i.change');
-icon.classList.remove('change');
+  const icon = document.querySelector('.section-1-icons i.change');
+  icon.classList.remove('change');
 
-if(i > icons.length){
-    icons[0].classList.add('change')
+  if (i > icons.length) {
+    icons[0].classList.add('change');
     i = 1;
-}else {
-    icon.nextElementSibling.classList.add('change')
-}
-},700)
-
+  } else {
+    icon.nextElementSibling.classList.add('change');
+  }
+}, 700);
 
 // burger button class toggle
 
 const burgetBtn = document.querySelector('.burger-button');
 const navList = document.querySelector('.nav--links');
+const navLinks = document.querySelectorAll('.nav--link');
 
-burgetBtn.addEventListener('click',() => {
-    burgetBtn.classList.toggle('active');
-    navList.classList.toggle('active')
-})
+burgetBtn.addEventListener('click', () => {
+  burgetBtn.classList.toggle('active');
+  navList.classList.toggle('active');
+
+
+    navLinks.forEach((navLink, index) => {
+
+        if(navLink.style.animation){
+            navLink.style.animation = '';
+        }else {
+            navLink.style.animation = `fadeLinks 1.5s ease  ${index / 8 + .1}s`;
+        }
+     
+    });
+  
+});
